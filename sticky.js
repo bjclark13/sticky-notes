06cf593d04e974c8f109aafbfb68b8ea41e87776
BJ Clark <bjclark13@outlook.com>
2021-09-02 18:31:21 -0400

    Add starter code

---
+++ sticky.js
@@ -0,0 +1,21 @@
+'use strict';
+{
+	const tasks = [
+		{
+			status: 'todo',
+			body: 'Clean Kitchen',
+		},
+		{
+			status: 'doing',
+			body: 'Learn DOM',
+		},
+		{
+			status: 'todo',
+			body: 'Eat Pizza',
+		},
+		{
+			status: 'done',
+			body: 'Make Coffee',
+		},
+	];
+}
