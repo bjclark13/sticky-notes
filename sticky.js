a3ae7bba67569ad16abe9dd55b977f734364ab6d
BJ Clark <bjclark13@outlook.com>
2021-09-02 20:25:55 -0400

    Add comments

--- sticky.js
+++ sticky.js
@@ -18,4 +18,11 @@
 			body: 'Make Coffee',
 		},
 	];
+
+	// TODO: add each task to the appropriate column
+
+    // TODO: add support for a "color" option
+
+    // TODO: single click should move to the right,
+    //       double click should move to the left
 }
